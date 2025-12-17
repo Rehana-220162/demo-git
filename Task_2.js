843cd603ad6e5f6718f21922c3021a862bce32fc
Rehana-220162 <n220162@rguktn.ac.in>
2025-12-17 15:04:28 +0530

    Added project setup

---
+++ Task_2.js
@@ -0,0 +1,10 @@
+function add(x,y){
+    return x+y;
+}
+const subtract=(x,y)=>x-y;
+let output1="";
+let output2="";
+output1 +=add(10,5);
+output2 +=subtract(10,5);
+console.log(output1);
+console.log(output2);
---
+++ Task_3.js
@@ -0,0 +1,3 @@
+alert("Hello! This is an alert meassage");
+let name=prompt("What is your name?");
+console.log(name);
