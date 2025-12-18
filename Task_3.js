76700b38770ff5990cda19f7de2d10a85293c756
Rehana-220162 <n220162@rguktn.ac.in>
2025-12-18 11:35:45 +0530

    Deleted multiple Files

--- Task_3.js
+++
@@ -1,3 +0,0 @@
-alert("Hello! This is an alert meassage");
-let name=prompt("What is your name?");
-console.log(name);
--- Task_4.js
+++
@@ -1,9 +0,0 @@
-function changeText(){
-    document.getElementById("text").innerHTML="The text changed!";
-}
-function addContent(){
-    document.getElementById("text").innerHTML+="<p>New content added!</p>";
-}
-function removeContent(){
-    document.getElementById("text").innerHTML="";
-}
